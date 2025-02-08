ba9a67e77aa45821b39306c40f5995b35f69744d
bryan <bryan@DESKTOP-OUICEDR>
2025-02-08 13:33:31 +0800

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+
+        public IActionResult Index1()
+        {
+            return View();
+
+        }
+        
+
+
+
+
+
+    }
+}
